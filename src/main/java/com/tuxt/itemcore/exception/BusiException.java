264001e3b72764ef2d0863a70fdd70ec56e4b636
tuxiantian <tuxiantian@163.com>
2016-11-23 19:49:50 +0800

    异常类的使用

---
+++ src/main/java/com/tuxt/itemcore/exception/BusiException.java
@@ -0,0 +1,32 @@
+package com.tuxt.itemcore.exception;
+public class BusiException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 260306311701422290L;
+	private String exceptionId;
+	
+	public String getExceptionId() {
+		return exceptionId;
+	}
+	public void setExceptionId(String exceptionId) {
+		this.exceptionId = exceptionId;
+	}
+	public BusiException(String message) {
+		super(message);
+	}
+	public BusiException(Throwable cause) {
+		super(cause);
+	}
+	public BusiException(String message,Throwable cause) {
+		super(message,cause);
+	}
+	public BusiException(String exceptionId,String message) {
+		super(message);
+		this.exceptionId=exceptionId;
+	}
+	public BusiException(String exceptionId,String message,Throwable cause) {
+		super(message,cause);
+		this.exceptionId=exceptionId;
+	}
+}
